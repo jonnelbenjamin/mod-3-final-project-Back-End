ac2cbf3ace25dfa8449b4960e9f42f52f330f3ec
github username <github email address>
2019-01-24 11:06:42 -0500

    Lots of back-end work

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,19 @@ class UsersController < ApplicationController
   def index
     render json: User.all
   end
+
+  def update
+    current_user = User.find(params[:id])
+    byebug
+    #we want to search for the corresponding url within topics from strong_params
+  end
+
+
+
+  private
+
+  def strong_params
+    params.require(:user).permit(:url, :author, :title, :description, :urlToImage)
+  end
+
 end
